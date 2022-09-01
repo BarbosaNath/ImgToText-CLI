dd4c238cc0d02edee7ab5adc53901685c879200c
Nath <playerxmatt@gmail.com>
2022-09-01 16:52:33 -0300

    CLI update

--- imgtotext-cli.py
+++ imgtotext-cli.py
@@ -1,4 +1,54 @@
 # Better CLI functions
-from rich import print
+import keyboard
+from rich.console import Console
+from rich.table import Table
+from imgtotext_lib import convert_image
+from rich import traceback
+from time import sleep
+import os # , sys
+traceback.install()
 
-print('TODO: CLI')
+console = Console()
+choice = 0
+
+def print_dir():
+    global choice
+    global console
+
+    console.clear()
+    directory = Table()
+    directory.add_column('Choose a directory or a file', justify='left')
+
+    dirs = os.listdir()
+
+    for i, dir in enumerate(dirs):
+        if i == choice:
+            directory.add_row(dir, style='cyan')
+        else:
+            directory.add_row(dir)
+    console.print(directory)
+
+print_dir()
+while True:
+    sleep(.05)
+    if keyboard.read_key() in ['w','up']:
+        choice -= 1
+        if choice < 0:
+            choice = len(os.listdir())-1
+        print_dir()
+
+    elif keyboard.read_key() in ['s','down']:
+        choice += 1
+        if choice > len(os.listdir()):
+            choice = 0
+        print_dir()
+    elif keyboard.is_pressed('esc'):
+        break
+    elif keyboard.is_pressed('enter'):
+        console.print('Reading File', style='bold cyan')
+        console.print(convert_image(os.listdir()[choice], './abc.txt', 'osd+equ+por'))
+        break
+
+
+
+
--- imgtotext_lib.py
+++ imgtotext_lib.py
@@ -4,16 +4,24 @@ pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesserac
 
 # Image prossecing
 from PIL import Image
+import os, platform
 
-def convert_image(image_path: str, text_path: str):
+def clear():
+    if platform.system().lower() == 'windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def convert_image(image_path: str, text_path: str, lang: str = 'eng'):
     # Open image
     img = Image.open(image_path)
     
     # Print image information (not necessary)
-    print(img)
+    # print(img)
 
-    result = pytesseract.image_to_string(img)
+    result = pytesseract.image_to_string(img, lang=lang, config='bazaar')
 
     with open(text_path, 'w') as file:
         file.write(result)
-        print(result)
+    
+    return result
